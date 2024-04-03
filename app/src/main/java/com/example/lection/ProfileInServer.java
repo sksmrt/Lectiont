13d35cbed339c838a4e17f02cfc71abf76f2b560
sksmrt <pankratovvaceslav89@gmail.com>
2024-04-03 12:50:10 +0700

    Create project

---
+++ app/src/main/java/com/example/lection/ProfileInServer.java
@@ -0,0 +1,14 @@
+package com.example.lection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileInServer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_in_server);
+    }
+}
---
+++ app/src/main/java/com/example/lection/RedactProfile.java
@@ -0,0 +1,14 @@
+package com.example.lection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RedactProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.redact_profile);
+    }
+}
